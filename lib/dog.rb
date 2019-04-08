95b1820ef8bdbaa54e0f49f60267b8afe6bf9dd5
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-08 17:36:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,6 @@
 # Your code goes here!
-class Dog = (name)
-  def name 
-    @name = name
-  end 
-  
-  def name 
-    @name 
-  end
-  
-  def Bark
-    puts "Oufff"
-  end
+class Dog 
+
 end 
 
 pops = Dog.new 
